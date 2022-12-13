b8ed41d7bfb8e20e6e8933e3178ec37521c983c7
yingcyl <chiayinglow@gmail.com>
2022-12-13 17:59:12 +0000

    Create axios get request with user's search term

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: { q: term },
+    });
+    console.log(response.data.items);
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        {this.state.videos.length} videos found
       </div>
     );
   }
