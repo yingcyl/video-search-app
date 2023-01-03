0ea9e1645270c9264b71fd4905fa0a5d6b487c45
yingcyl <chiayinglow@gmail.com>
2023-01-03 16:41:42 +0000

    Create and style VideoItem component
    -Map each video in VideoList to a VideoItem component
    -Include video thumbnail and title in VideoItem
    -Style list using css

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,13 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
 // props destructured so props doesn't need to be referenced each time
 const VideoList = ({ videos }) => {
-  return <div>{videos.length}</div>;
+  // for each video in videos, VideoItem component will be returned
+  const renderedList = videos.map((video) => {
+    return <VideoItem video={video} />;
+  });
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
