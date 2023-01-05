96545d16bada03cd3dfb95be88207d3e1e41f55b
yingcyl <chiayinglow@gmail.com>
2023-01-05 15:42:10 +0000

    Style layout using Semantic UI grid

--- src/components/App.js
+++ src/components/App.js
@@ -23,12 +23,21 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        {/* passing videos state as a prop to VideoList */}
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              {" "}
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              {/* passing videos state as a prop to VideoList */}
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
